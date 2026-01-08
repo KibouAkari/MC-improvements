c58ba41a541c07c474b7e6a4a6d26cc3f7621ce1
KibouAkari <89930060+KibouAkari@users.noreply.github.com>
2026-01-08 22:07:10 +0100

    Create mc-improvements.java

---
+++ mc-improvements.java
@@ -0,0 +1,10 @@
+package de.example.serverrules;
+
+
+import net.neoforged.fml.common.Mod;
+
+
+@Mod(ServerRulesMod.MOD_ID)
+public class ServerRulesMod {
+public static final String MOD_ID = "serverrules";
+}
